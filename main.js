de8e501006838654a5d94740212d3ffcaf585068
PRASASTIRAI <77830876+PRASASTIRAI@users.noreply.github.com>
2021-08-14 21:15:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function first(){
+    window.location="bedRoom.html";
+}
+
+function second(){
+    window.location="desk.html";
+}
+function third(){
+    window.location="fruitBasket.html";
+}
+function fourth(){
+    window.location="T.V.andA.C.html";
+}
+function fifth(){
+    window.location="bottles.html";
+}
